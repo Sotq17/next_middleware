d54bc8a1409acad76a8574abf0a9075efd805c59
高橋　宗太 <souta_takahashi@012grp.co.jp>
2021-11-02 11:05:29 +0900

    no message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
+
+
+
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
