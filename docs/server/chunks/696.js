7d23625982e8cab9f937c80b1e035be39f6a4ec1
高橋　宗太 <souta_takahashi@012grp.co.jp>
2021-11-02 10:39:48 +0900

    buildをローカルで試す（ついでにGithubPagesにあげてみる）

---
+++ docs/server/chunks/696.js
@@ -0,0 +1,25 @@
+exports.id = 696;
+exports.ids = [696];
+exports.modules = {
+
+/***/ 696:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"container": "Home_container__sDexO",
+	"main": "Home_main__1gTMt",
+	"footer": "Home_footer__34ULc",
+	"title": "Home_title__38XO6",
+	"description": "Home_description__p2VX9",
+	"code": "Home_code__7lt7E",
+	"grid": "Home_grid__2Clz5",
+	"card": "Home_card__2kgLM",
+	"logo": "Home_logo__qxZJS"
+};
+
+
+/***/ })
+
+};
+;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  distDir: 'docs',
+  trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
 }
